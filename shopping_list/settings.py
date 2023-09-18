2983a41d723d2d7b57c064fa37ed73e5d6268be0
zaimnazif974 <zaim.aydin@ui.ac.id>
2023-09-18 16:22:31 +0700

    Fixing deployment

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -15,7 +15,6 @@ import environ
 import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 env = environ.Env()
 
 # Quick-start development settings - unsuitable for production
