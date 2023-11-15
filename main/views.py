2b429fd240d9c122261d793f7552bab434738594
zaimnazif974 <zaim.aydin@ui.ac.id>
2023-11-15 14:26:32 +0700

    minor change for deployment

--- main/views.py
+++ main/views.py
@@ -119,8 +119,8 @@ def add_product_ajax(request):
         description = request.POST.get("description")
         user = request.user
 
-        new_product = Product(name=name, price=price, description=description, user=user)
-        new_product.save()
+        produk_baru = Product(name=name, price=price, description=description, user=user)
+        produk_baru.save()
 
         return HttpResponse(b"CREATED", status=201)
     return HttpResponseNotFound()
