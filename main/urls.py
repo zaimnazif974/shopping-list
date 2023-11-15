ddb139bee89b02d210172607b4f3b5278aa70889
zaimnazif974 <zaim.aydin@ui.ac.id>
2023-11-15 15:34:04 +0700

    redeploy utk keperluan tutorial08 1

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product
-from main.views import get_product_json, add_product_ajax
+from main.views import get_product_json, add_product_ajax, create_product_flutter
 
 app_name = 'main'
 
@@ -17,5 +17,6 @@ urlpatterns = [
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'),
     path('get-product/', get_product_json, name='get_product_json'),
-    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
